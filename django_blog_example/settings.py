5e2c55c006adb37a4fac451d3bd565a0a28ab90f
Mauricio Aizaga <mauricioaizaga@gmail.com>
2013-09-26 09:58:08 -0500

    database on git

--- django_blog_example/settings.py
+++ django_blog_example/settings.py
@@ -16,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db.sqlite',                      # Or path to database file if using sqlite3.
+        'NAME': 'db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
